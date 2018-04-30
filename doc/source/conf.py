f988926177b66479d15a77d134b1ef7fd59b7a14
Paul B. Manis <pmanis@med.unc.edu>
2018-04-30 08:17:13 -0400

    try fixing rtd_documentation issue (adding more modules to mock list)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'neuron', 'argparse']
+MOCK_MODULES = ['numpy', 'scipy', 'pyqtgraph', 'matplotlib', 'neuron', 'argparse']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
